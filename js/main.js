bc1527259a12b91fa628aa0d007a87e8c8dee654
LasaroAlmeida <lasarodalmeida@gmail.com>
2023-06-20 20:54:46 -0300

    adciona estado

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
+import { getTabuleiro } from "../stage.js";
+
 // drag and drop api
-const casas  =  document.querySelectorAll(".casa");
+const eTabuleiro  =  document.querySelector(".tabuleiro");
 const disco = document.querySelector(".disco");
 
-console.log(disco);
 
-disco.addEventListener("dragstart", comecaArrastar);
-for (let i = 0; i <casas.length; i++) {
-    const casa = casas[i];
-    casa.addEventListener("dragover", passouPorCima);
-    casa.addEventListener("drop", recebeAlgo);
 
-}
 let arrastado = null;
-
+setup();
 
 function comecaArrastar(event) {
    // console.log(event.target);
@@ -29,4 +24,39 @@ function recebeAlgo(event) {
 function passouPorCima(event) {
     event.preventDefault();
     //console.log(event.target);
+}
+
+
+function setup(){
+    const tabuleiro = getTabuleiro();
+    for (let i = 0; i < tabuleiro.length; i++) {
+        const casa = tabuleiro[i];
+        const eCasa = criaCasa(casa);
+        eTabuleiro.appendChild(eCasa);
+    }
+}
+
+function criaCasa(casa){
+    const eCasa = document.createElement("div");
+    eCasa.classList.add("casa");
+    eCasa.addEventListener("dragover", passouPorCima);
+    eCasa.addEventListener("drop", recebeAlgo);
+    if (casa) {
+        const eDisco = criaDisco(casa);
+        eCasa.appendChild(eDisco);
+    }
+    return eCasa;
+}
+
+function criaDisco(casa){
+    const eDisco = document.createElement("div");
+    eDisco.classList.add("disco");
+    eDisco.addEventListener("dragstart", comecaArrastar);
+    if (casa=== 'p'){
+        eDisco.classList.add("preto");
+    }
+    else{
+        eDisco.classList.add("branco");
+    }
+    return eDisco;
 }
